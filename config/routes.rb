104d43112c18dbdd7f8d05829963fc8a295c097b
AgustinPalmaM <palma.agustin7@gmail.com>
2023-10-10 16:57:21 -0300

    Add user controller with index method

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  # get 'user/index'
   get 'home/index'
   devise_for :users
+  resources :users, only: %i[index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.
 
   # Defines the root path route ("/")
