9b33a94cb520bd66475b749f47045e82f9dcef6d
Giovanny Salcedo <gsalcedo26@gmail.com>
2023-10-13 00:09:37 -0500

    added gem annotate

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,23 @@
+# == Schema Information
+#
+# Table name: session_qas
+#
+#  id         :bigint           not null, primary key
+#  body       :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  product_id :bigint           not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_session_qas_on_product_id  (product_id)
+#  index_session_qas_on_user_id     (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (product_id => products.id)
+#  fk_rails_...  (user_id => users.id)
+#
 class Question < SessionQa
 end
--- test/models/favorite_test.rb
+++ test/models/favorite_test.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: favorites
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  product_id :bigint           not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_favorites_on_product_id  (product_id)
+#  index_favorites_on_user_id     (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (product_id => products.id)
+#  fk_rails_...  (user_id => users.id)
+#
 require "test_helper"
 
 class FavoriteTest < ActiveSupport::TestCase
